dd3eec6d727a162097f6eb071e33bd438ddda016
George <georgesantos11@protonmail.com>
2019-10-03 11:07:03 -0300

    Util script added to init database for guilds.

--- modules/level/leveller.js
+++ modules/level/leveller.js
@@ -3,16 +3,19 @@ const Module = include("src/structures/Module");
 module.exports = class Guild extends Module {
     constructor(...args) {
         super(...args, {
-            name: "guild",
+            name: "guild:leveller",
             events: {
                 messageCreate: "context"
             }
         })
+        this.controller = new Map();
     }
     async init() {
         this.data  = await this._client.plugins.get("db").models;
     }
     async context(message) {
+        
+        if (!message.channel.guild) return;
         const store = await this.data.guilds.cache().findByPk(message.channel.guild.id);
         /**
          * Checking if the member was in the DB. If not, create the default xp-card.
@@ -21,16 +24,18 @@ module.exports = class Guild extends Module {
         const members = store.levelSystem.members;
         const member  = members.find(m => m.id === message.author.id);
 
+        
+
         if (!member) {
-            members.push({ "id": message.author.id, "experience": 0, "level": 0, "needed": 163 });
+            members.push({ "id": message.author.id, "experience": 0, "level": 0, "" });
             await store.update({ "levelSystem.members": members });
             return store.save();
         }
-
-        console.log(member);
-
     }
     async addExp(target, store) {
         
+    }
+    async getNeeded(level) {
+        return 3.5 * (Math.pow(level, 2)) + 60 * level + 100;
     }
 }
