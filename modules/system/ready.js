1a6159efc6a4de569e68d1540b440d441ae427e6
George <georgesantos11@protonmail.com>
2019-10-03 15:49:00 -0300

    Some boring stuff

--- modules/system/ready.js
+++ modules/system/ready.js
@@ -9,5 +9,13 @@ module.exports = class Ready extends Module {
     }
     connected() {
         this.logger.debug("I'm ready, sir!");
+
+        /**
+         * @GuildSetup
+         * const db = this._client.plugins.get("db");
+         * this._client.guilds.forEach(g => { 
+         *   db.models.guilds.cache().create({ id: g.id, name: g.name });
+         * })
+            */
     }
 }
