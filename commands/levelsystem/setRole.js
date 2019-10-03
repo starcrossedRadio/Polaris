ae5eef750cf4d1ea9ace79f60fb4bdb71c9304cd
George <georgesantos11@protonmail.com>
2019-10-03 11:03:15 -0300

    Added database connection.

---
+++ commands/levelsystem/setRole.js
@@ -0,0 +1,36 @@
+const Command = include("src/structures/Command");
+
+module.exports = class Avatar extends Command {
+    constructor(...args) {
+        super(...args, {
+            name: 'setlevel',
+            aliases: ['slvl'],
+            group: 'levelsystem',
+            usage: [{
+                name: 'member',
+                displayName: 'id/mention/username',
+                type: 'member',
+                optional: true
+            }],
+            options: { guildOnly: true, localeKey: 'commands' },
+            cooldown: 5
+        })
+    }
+    async handle({ msg, args, client }, responder) {
+
+        let user = args.member ?
+            msg.channel.guild.members.get(args.member[0].id).user :
+            msg.channel.guild.members.get(msg.member.user.id).user;
+
+        const embed = new client.embed
+        embed
+            .title(`${user.username}'s Avatar!`)
+            .description(responder.t("{{download_avatar}}", { url: user.dynamicAvatarURL(null, 512) }))
+            .color(11220318)
+            .image(user.dynamicAvatarURL(null, 512))
+            .footer(`${msg.author.username}#${msg.author.discriminator}`, msg.author.avatarURL)
+            .timestamp()
+
+        responder.embed(embed).send()
+    }
+}
