2fffe6209501701bc6c3fa0005df9ee718d798a3
a <b>
2019-10-21 12:21:41 -0300

    Added new options and methods.

--- commands/settings/welcome.js
+++ commands/settings/welcome.js
@@ -14,15 +14,23 @@ module.exports = class Avatar extends Command {
             mapFunc: ch => responder.t(`{{welcome.${ch}}}`)
         }).then(arg => arg.length ? this[arg[0]](container, responder) : false)
     }
+    async get({msg, store}, responder) {
+      if (!store.inframodules.welcome) {
+        responder.error(responder.t("{{welcome.rejectMessageRequest}}"));
+        return 0;
+      } else {
+        responder.format("emoji:info").send(store.inframodules.welcome.message);
+      }
+    }
     async set ({ msg, args, store, client }, responder) {
         responder.selection(["edit", "delete"], {
             title: "{{welcome.configure.title}}",
             mapFunc: ch => responder.t(`{{welcome.configure.${ch}}}`)
         }).then(arg => arg.length ? this[arg[0]]({ msg, args, store, client}, responder) : false);
-    }
+      }
     async edit ({ msg, args, store, client }, responder) {
       responder.format("emoji:info").dialog([{ prompt:responder.t("{{welcome.configure.edit_prompt.typerequest}}", { user: msg.author.id }), input: {type: "string", name: "message"}}]).then(args => {
-        const message = args.message;
+       const message = args.message;
         if(message.length < 15) {
           responder.error(responder.t("{{welcome.configure.edit_prompt.lessThan}}"));
           return 0;
@@ -32,12 +40,32 @@ module.exports = class Avatar extends Command {
           return 0;
         }
        if(!store.inframodules.welcome) {
-         store.inframodules.welcome = { channel: msg.channel.id, message: message };
-         store.save();
+         store.cache().update({ "inframodules.welcome": { "channel": msg.channel.id, "message": message  }  });
+         store.cache().save().then(() => {
+           responder.success(responder.t("{{welcome.configure.edit_prompt.success}}"));
+           return 1;
+         })
        } else {
-         store.inframodules.welcome.message = message;
-         store.save();
+         store.cache().update({ "inframodules.welcome": { "message": message  }  });
+         store.cache().save().then(() => {
+           responder.success(responder.t("{{welcome.configure.edit_prompt.success}}"));
+           return 1;
+         })
        }
+      })
+    }
+    async delete({store}, responder) {
+      const inframodules = store.inframodules;
+      if (!inframodules.welcome) {
+        responder.error(responder.t("{{welcome.delete.reject}}"));
+        return 0;
+      }
+      delete inframodules.welcome;
+      store.update({ "inframodules": inframodules });
+      store.cache().save().then(() => {
+        store.save();
+        responder.success(responder.t("{{welcome.delete.success}}"));
+        return 1;
       })
     }
 }
